902bee2472f45460ab6ad2566f0de09e756cbb30
yellowxuchenfeng <401120833@qq.com>
2019-01-26 13:45:41 +0800

    33

---
+++ src/com/xcf/entity/AAA.java
@@ -0,0 +1,7 @@
+package com.xcf.entity;
+
+/**
+ * Created by dell on 2019/1/26.
+ */
+public class AAA {
+}
---
+++ src/com/xcf/entity/User.java
@@ -0,0 +1,8 @@
+package com.xcf.entity;
+
+/**
+ * Created by dell on 2019/1/26.
+ */
+public class User {
+    private String name;
+}
